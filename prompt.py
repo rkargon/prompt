896529ea70f9eeb76dbe6d5663d488a9d3f87469
Raphael Kargon <raphael.kargon@gmail.com>
2015-07-31 21:23:34 -0400

    added tree data structure for parsing strings

--- prompt.py
+++ prompt.py
@@ -1,5 +1,15 @@
 import sys
 
+class ParseTree:
+	def __init__(self):
+		self._parent = None
+		self._children = []
+		self.string = ""
+
+	def add_child(self, subtree):
+		subtree._parent = self
+		self._children += subtree
+
 def main(argv):
 	if len(argv) != 2:
 		return usage()
@@ -8,8 +18,25 @@ def main(argv):
 	sys.stdout.write(prompt)
 
 def parse(pattern):
-	# TODO actually do something
+	parse_tree = generate_parse_tree(pattern)
+	out_str = expand_directives(parse_tree)
 	return pattern
+
+def generate_parse_tree(pattern):
+	root = ParseTree()
+	current_node = root
+	for char in pattern:
+		if char == "{":
+			child_node = ParseTree(current_node)
+			current_node
+		elif char == "}":
+			pass
+		else:
+			current_node.string += char
+	pass
+
+def expand_directives(parse_tree):
+	pass
 
 def usage():
 	print "prompt.py <pattern>"
